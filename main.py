c8245cc062ca40299a9658c0e87b632fff7f8033
MalavikaGowthaman <malavikag115@gmail.com>
2024-10-02 19:59:29 -0400

    code added

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from classifier import logger
 from classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-
+from classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from classifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
+from classifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -21,6 +23,31 @@ try:
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+    
+    
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
 except Exception as e:
         logger.exception(e)
         raise e
--- src/classifier/config/configuration.py
+++ src/classifier/config/configuration.py
@@ -3,8 +3,8 @@ from classifier.constants import *
 from classifier.utils.common import read_yaml, create_directories
 from classifier.entity.config_entity import (DataIngestionConfig,
                                              PrepareBaseModelConfig,
-                                            #  TrainingConfig,
-                                            #  EvaluationConfig
+                                             TrainingConfig,
+                                             EvaluationConfig
                                             )
 
 class ConfigurationManager:
@@ -52,3 +52,37 @@ class ConfigurationManager:
         )
 
         return prepare_base_model_config
+    
+    def get_training_config(self) -> TrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.prepare_base_model
+        params = self.params
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "train")
+        create_directories([
+            Path(training.root_dir)
+        ])
+
+        training_config = TrainingConfig(
+            root_dir=Path(training.root_dir),
+            trained_model_path=Path(training.trained_model_path),
+            updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
+            training_data=Path(training_data),
+            params_epochs=params.EPOCHS,
+            params_batch_size=params.BATCH_SIZE,
+            params_is_augmentation=params.AUGMENTATION,
+            params_image_size=params.IMAGE_SIZE
+        )
+
+        return training_config
+    
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/train",
+            mlflow_uri="https://dagshub.com/MalavikaGowthaman/End-to-End-alzheimer_disease.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
+    
